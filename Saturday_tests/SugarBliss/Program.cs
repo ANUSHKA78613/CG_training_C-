cd20077041dba5e9649ec4a7b8d7f4f5831091b3
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-01-10 15:51:05 +0530

    Add files via upload

---
+++ Saturday_tests/SugarBliss/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http.Headers;
+// using Security.Authentication;
+class Program
+{
+     public Chocolate CalculateDiscountedPrice(Chocolate chocolate)
+    {
+        
+        if(chocolate.Flavour == "Dark")
+        {
+            chocolate.TotalPrice = chocolate.Quantity*chocolate.PricePerUnit;
+            chocolate.DiscountedPrice = chocolate.TotalPrice - (chocolate.TotalPrice*18/100);
+        }
+        else if(chocolate.Flavour == "Milk")
+        {
+              chocolate.TotalPrice = chocolate.Quantity*chocolate.PricePerUnit;
+            chocolate.DiscountedPrice = chocolate.TotalPrice - (chocolate.TotalPrice*12/100);
+        }
+        else if(chocolate.Flavour == "White")
+        {
+             chocolate.TotalPrice = chocolate.Quantity*chocolate.PricePerUnit;
+            chocolate.DiscountedPrice = chocolate.TotalPrice - (chocolate.TotalPrice*6/100);
+        }
+        else
+        {
+            Console.WriteLine("invalid flavour");
+        }
+        return chocolate;
+    }
+    public static void Main()
+    {
+    Console.WriteLine("enter the f: ");
+    string f = Console.ReadLine();
+    Console.WriteLine("Enter q: ");
+    int q = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter  ppu: ");
+    int ppu = int.Parse(Console.ReadLine());
+   
+Chocolate chocolate = new Chocolate
+{
+    Flavour = f,Quantity = q, PricePerUnit = ppu
+};
+if(chocolate.ValidateChocolateFlavour() == false){Console.WriteLine("Invalid flavour");}
+        else
+        {
+             Program p = new Program();
+            chocolate = p.CalculateDiscountedPrice(chocolate);
+              Console.WriteLine("Flavour : " + chocolate.Flavour);
+            Console.WriteLine("Quantity : " + chocolate.Quantity);
+            Console.WriteLine("Price Per Unit : " + chocolate.PricePerUnit);
+            Console.WriteLine("Total Price : " + chocolate.TotalPrice);
+            Console.WriteLine("Discounted Price : " + chocolate.DiscountedPrice);
+   }
+
+    }
+}
---
+++ Saturday_tests/SugarBliss/assignment.cs
@@ -0,0 +1,17 @@
+using System;
+class Chocolate
+{
+    public string Flavour{get;set;}
+    public int Quantity{get;set;}
+    public int PricePerUnit{get;set;}
+    public double TotalPrice{get;set;}
+    public double DiscountedPrice{get;set;}
+    
+public bool ValidateChocolateFlavour()
+    {
+        if(Flavour == "Dark"  || Flavour == "Milk" || Flavour == "white"){return true;}
+        return false;
+    }
+
+
+}

2c979cf1caa7be446c32447b36f1e4f7644fa147
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-01-30 12:24:40 +0530

    Add files via upload

---
+++ TOP_BRAIN/Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main()
+    {
+        Dictionary<int,int> d = new Dictionary<int, int>();
+        d.Add(1,20000);
+        d.Add(4,40000);
+        d.Add(5,15000);
+        int s = 0;
+        foreach(var i in d)
+        {
+            s += i.Value;
+            
+        }
+        Console.WriteLine(s);
+
+    }
+}

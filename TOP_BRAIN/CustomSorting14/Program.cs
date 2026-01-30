ac681c66118110da3c1e223cc49d5c3502555095
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-01-30 10:05:03 +0530

    Add files via upload

---
+++ TOP_BRAIN/CustomSorting14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+public class Student
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public int Marks { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Anu", Age = 22, Marks = 90 },
+            new Student { Name = "Ravi", Age = 20, Marks = 95 },
+            new Student { Name = "Neha", Age = 19, Marks = 90 }
+        };
+        students.Sort((x, y) =>
+        {
+            int markCompare = y.Marks.CompareTo(x.Marks); 
+            if (markCompare != 0)
+                return markCompare;
+            return x.Age.CompareTo(y.Age);
+        });
+        //  var sortedStudents = students.OrderByDescending(s => s.Marks).ThenBy(s => s.Age).ToList();
+        // foreach (var s in sortedStudents)
+        // {
+        //     Console.WriteLine($"{s.Name} - Age: {s.Age}, Marks: {s.Marks}");
+
+        foreach (var s in students)
+        {
+            Console.WriteLine($"{s.Name} - Age = {s.Age} - Marks = {s.Marks}");
+        }
+    }
+}
---
+++ TOP_BRAIN/MidRouting/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        double r = Convert.ToDouble(Console.ReadLine());
+        double area = 3.14 * r * r;
+        Console.WriteLine($"area = {area:F2}");
+    }
+}

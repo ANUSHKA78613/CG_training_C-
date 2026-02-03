4f6f2f910e2cbcad2ae9913d9c579147b19cae3b
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-02-03 09:33:21 +0530

    Add files via upload

---
+++ TOP_BRAIN/C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+class Program
+{
+    public static bool IsVowel(char ch)
+    {
+        return "aeiou".Contains(ch);
+    }
+    public static void Main()
+    {
+        string first = Console.ReadLine();
+        string second = Console.ReadLine();
+
+        string secondLower = second.ToLower();
+        var filtered = first.Where(ch => IsVowel(char.ToLower(ch)) || !secondLower.Contains(char.ToLower(ch))).ToList();
+        var result = filtered.Where((ch,i) => i == 0 || ch != filtered[i-1]);
+        Console.WriteLine(new string(result.ToArray()));
+    }
+}

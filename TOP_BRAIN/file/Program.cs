5bb194e6f846206e39413c47cdd395439229f99a
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-02-05 09:31:15 +0530

    Add files via upload

---
+++ TOP_BRAIN/file/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = "log.txt";
+        string outputFile = "error.txt";
+
+        if (!File.Exists(inputFile))
+        {
+            File.Create(inputFile).Close();
+        }
+
+        if (!File.Exists(outputFile))
+        {
+            File.Create(outputFile).Close();
+        }
+
+        string[] lines = File.ReadAllLines(inputFile);
+
+        using (StreamWriter writer = new StreamWriter(outputFile, false))
+        {
+            foreach (string line in lines)
+            {
+                if (line.IndexOf("ERROR", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}

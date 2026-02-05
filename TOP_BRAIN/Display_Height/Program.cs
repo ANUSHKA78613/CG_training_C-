335a7e211c4d43010db1f894cb96d522e8705ca8
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-02-05 15:37:31 +0530

    Add files via upload

---
+++ TOP_BRAIN/Display_Height/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int height = Convert.ToInt32(Console.ReadLine());
+        if(height < 150)
+        {
+            Console.WriteLine("Short");
+        }
+        else if(height >= 150 && height < 180)
+        {
+            Console.WriteLine("Average");
+        }
+        else
+        {
+            Console.WriteLine("Tall");
+        }
+    }
+}

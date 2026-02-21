512bbbee2761f8295bec1cc35020165649655861
Anushka Lodhi <anushkalodhi785@gmail.com>
2026-02-21 15:34:04 +0530

    Add files via upload

---
+++ TOP_BRAIN/top_brain_q9(databaseadapter)/top_brain_q9(databaseadapter)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString =
+            "Data Source=ANUSHKA\\SQLEXPRESS;Initial Catalog=COLLEGE;Integrated Security=True";
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Students", con);
+
+          
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);
+
+           
+            DataSet ds = new DataSet();
+
+         
+            da.Fill(ds, "Students");
+
+            Console.WriteLine("Data Loaded Successfully!");
+
+            DataTable table = ds.Tables["Students"];
+
+           
+            if (table.Rows.Count > 0)
+            {
+                table.Rows[0]["Name"] = "Anushka";
+                table.Rows[0]["Age"] = 23;
+            }
+
+          
+            DataRow newRow = table.NewRow();
+            newRow["Name"] = "Riya";
+            newRow["Age"] = 21;
+            table.Rows.Add(newRow);
+
+            if (table.Rows.Count > 1)
+            {
+                table.Rows[1].Delete();
+            }
+
+         
+            da.Update(ds, "Students");
+
+            Console.WriteLine("Database Updated Successfully!");
+
+            Console.ReadLine();
+        }
+    }
+}
